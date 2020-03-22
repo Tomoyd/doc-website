691f48ec55ec6adbcc7bb832bcb2ed2d4663a5b4
tomoyd <ydh9419@163.com>
2020-03-23 05:01:52 +0800

    表格

--- src/pages/TableComponent.js
+++ src/pages/TableComponent.js
@@ -1,5 +1,5 @@
 import React,{Component} from "react"
-import { Table,Button } from 'antd';
+import { Table,Button,Modal,Form,Input } from 'antd';
 
 
 
@@ -25,7 +25,15 @@ class TableComponent extends Component {
     state = {
         selectedRowKeys: [], // Check here to configure the default column
         data:data.slice(),
+        visible:false,
+        formInfo:{
+            name:"",
+            age:"",
+            address:"",
+        },
+
     };
+    form=null
     onSelectChange = selectedRowKeys => {
         this.setState({ selectedRowKeys });
     };
@@ -49,8 +57,11 @@ class TableComponent extends Component {
             render: (text, record, index) => { return <Button onClick={ ()=>{this.deleteItem(index)}}>删除</Button>},
         },
     ];
-
-
+    handleCancel(){
+        this.setState({
+            visible:false
+        })
+    }
     render() {
         const { selectedRowKeys } = this.state;
         const rowSelection = {
@@ -90,7 +101,55 @@ class TableComponent extends Component {
                 },
             ],
         };
-        return <Table rowSelection={rowSelection} columns={this.columns} dataSource={this.state.data} />;
+        return <div>
+            <div><Button style={{marginBottom:"10px"}} onClick={()=>{this.setState({visible:true})}}>添加</Button></div>
+            <Table rowSelection={rowSelection} columns={this.columns} dataSource={this.state.data} />
+            <Modal
+                title="添加"
+                visible={this.state.visible}
+                onOk={() => {
+                    let datas=this.state.data.slice()
+                    let newItem={...this.state.formInfo,key:datas.length}
+                    datas.unshift(newItem)
+                    this.setState({
+                        data:datas,
+                        visible:false
+                    })
+                }}
+                onCancel={this.handleCancel.bind(this)}
+            >
+                <Form
+                    onValuesChange={(changeValues,allValues)=>{
+                        this.setState({
+                            formInfo:{...this.state.formInfo,...allValues}
+                        })
+                    }}
+                    name="basic"
+                >
+                    <Form.Item
+                        label="名称"
+                        name="name"
+                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input/>
+                    </Form.Item>
+                    <Form.Item
+                        label="年龄"
+                        name="age"
+                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input/>
+                    </Form.Item>
+                    <Form.Item
+                        label="地址"
+                        name="address"
+                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input/>
+                    </Form.Item>
+                </Form>
+            </Modal>
+        </div>;
     }
 }
 export default TableComponent;
