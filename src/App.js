3c403e0fee5e3154e0f85c97e683ddb10d7ccdf2
tomoyd <ydh9419@163.com>
2020-03-23 06:20:37 +0800

    build

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <Router>
             <Switch>
               <Route
-                  path="login"
+                  path="/login"
                   component={Login}
               >
               </Route>
