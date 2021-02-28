df66d972f9e42b357119d898f9c7b5f708e34c52
tomoyd <ydh9419@163.com>
2021-02-28 22:04:59 +0800

    [NFF]ts

---
+++ docs/ts/ts基础类型.md
@@ -0,0 +1,10 @@
+---
+id: base
+title: ts基础类型
+---
+
+:::note 定义
+TypeScript 是 JavaScript 的超集，
+扩展了 JavaScript，捕捉错误提供解释，任何 JavaScript，TypeScript 都可以编译后运行
+
+:::
--- sidebars.js
+++ sidebars.js
@@ -2,14 +2,17 @@ const transformToValidPageId = (ids, dir) =>
   dir ? ids.map((i) => `${dir}/${i}`) : ids;
 
 const FRONT_BASE = "front_base";
+const TS = "ts";
 module.exports = {
   someSidebar: {
     开始: ["home"],
     前端技术: [
       { 前端基础: transformToValidPageId(["js", "css", "html5"], FRONT_BASE) },
       "React",
-      "Vue"
+      "Vue",
+      { TS: transformToValidPageId(["base"], TS) }
     ],
+    部署: ["nginx", "webHook"],
     前端工程: ["mdx"],
     杂谈: ["speaking"]
   }
